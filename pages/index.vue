cd2f8a78dc57e93ff2d14cf9a868b64cd508f54e
s17001 <s17001@std.it-college.ac.jp>
2019-05-29 19:10:21 +0900

    getしたものを描画するようにした

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,20 @@
 <template>
   <section class="container">
     <div>
+      <h3>Nuxt.jsのタグがついた投稿だよ😉</h3>
+
+      <ul>
+        <li v-for="item in items" :key="item.id">
+
+          <h4>
+            <span>{{item.title}}</span>
+            <small>by {{item.user.id}}</small>
+          </h4>
+          <div>{{item.body.slice(0,130)}}......</div>
+          <p><a :href="item.url">{{item.url}}</a></p>
+
+        </li>
+      </ul>
 
     </div>
   </section>
@@ -10,11 +24,13 @@
 import AppLogo from '~/components/AppLogo.vue'
 
 export default {
-  async mounted() {
-    console.log(
-      JSON.stringify(
-        await this.$axios.$get('https://qiita.com/api/v2/items?query=tag:nuxt.js'), true, ' ')
-    )
+  async asyncData({ app }) {
+    const items = await app.$axios.$get('https://qiita.com/api/v2/items?query=tag:nuxt.js')
+
+    return {
+      items
+    }
+
   }
 }
 </script>
