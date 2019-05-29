2bf82139b0694dbd643df14dc3e3ba870b5151a1
s17001 <s17001@std.it-college.ac.jp>
2019-05-29 19:56:13 +0900

    _idが動かず

--- pages/users/_id.vue
+++ pages/users/_id.vue
@@ -1,17 +1,33 @@
 <template>
-  <dev>
-    <p>
-      /users/_id.vue
-    </p>
-  </dev>
+  <section class="container">
+    <dev>
+      <h3>{{user.id}}</h3>
+      <img :src="user.profile_image_url" width="120" alt="">
+      <p>{{user.description || 'No description'}}</p>
+      <p>
+        <nuxt-link to="/">
+          <small><b>トップへ戻る</b></small>
+        </nuxt-link>
+      </p>
+      <h3>{{user.id}}さんの投稿の一覧</h3>
+      <ul>
+        <li v-for="item in items" :key="item.id">
+          <h4><span>{{item.title}}</span></h4>
+          <div>{{item.body.slice(0,130)}}........</div>
+          <p><a target="_blank" :href="item.url"></a></p>
+        </li>
+      </ul>
+    </dev>
+  </section>
 </template>
 
 <script>
   export default {
-    data () {
-      return {
-        userId : this.$route.params.id
-      }
+    async asyncData({ route, app }) {
+      const user = await app.$axios.$get(`https://qiita.com/api/v2/users/${route.params.id}`)
+      const items = await app.$axios.$get(`https://qiita.com/api/v2/items?query=user:${route.params.id}`)
+
+      return {user,items}
     }
   }
 </script>
